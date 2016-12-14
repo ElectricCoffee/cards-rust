a5f07b034ad749bb3e0a35bc0ed6015fa4c751a5
Niko Lepka <slench102+git@gmail.com>
2016-12-14 09:35:32 +0100

    Change to use String instead of str and enum

--- src/cards.rs
+++ src/cards.rs
@@ -2,50 +2,51 @@ extern crate rand;
 use ::std::str;
 use self::rand::Rng;
 
-#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
-pub enum Suit { Clubs, Diamonds, Hearts, Spades, Stars }
-
 #[derive(Debug, Eq, PartialEq, PartialOrd)]
-pub struct Card<'a> {
-    suit: Suit,
-    value: &'a str,
+pub struct Card {
+    suit:  String,
+    value: String,
 }
 
-impl<'a> Card<'a> {
-    pub fn new(suit: Suit, value: &'a str) -> Card {
-        Card { suit: suit, value: value }
+impl Card {
+    pub fn new(suit: String, value: String) -> Card {
+        Card { suit: suit, value: value.to_string() }
+    }
+
+    pub fn new_from_str(suit: &str, value: &str) -> Card {
+        Card::new(suit.to_string(), value.to_string())
     }
 }
 
-impl<'a> Ord for Card<'a> {
+impl Ord for Card {
     fn cmp(&self, other: &Self) -> ::std::cmp::Ordering {
         self.value.cmp(&other.value)
     }
 }
 
-pub fn make_standard_deck<'a>() -> Vec<Card<'a>> {
+pub fn make_standard_deck() -> Vec<Card> {
     let mut result = Vec::new();
-    let suits  = &[Suit::Clubs, Suit::Diamonds, Suit::Hearts, Suit::Spades];
+    let suits  = &["♣", "♦", "♥", "♠"];
     let values = &["2", "3", "4", "5", "6", "7", "8", "9", "10",
                    "Jack", "Queen", "King", "Ace"];
 
     for suit in suits {
         for value in values {
-            result.push(Card::new(*suit, value));
+            result.push(Card::new_from_str(*suit, value));
         }
     }
     return result;
 }
 
-pub fn make_fifth_dimension_deck<'a>() -> Vec<Card<'a>> {
+pub fn make_fifth_dimension_deck<'a>() -> Vec<Card> {
     let mut result = Vec::new();
-    let suits  = &[Suit::Clubs, Suit::Diamonds, Suit::Hearts, Suit::Spades, Suit::Stars];
+    let suits  = &["♣", "♦", "♥", "♠", "★"];
     let values = &["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
                    "Jack", "Princess", "Queen", "King", "Joker"];
 
     for suit in suits {
         for value in values {
-            result.push(Card::new(*suit, value));
+            result.push(Card::new_from_str(*suit, value));
         }
     }
     return result;
