894a865d895bc75b0893549349cc29d4b0cf5fb9
electric coffee <slench102@gmail.com>
2016-12-18 15:06:03 +0100

    change from Vec to std::collections::VecDeque

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 use std::str;
 use std::fmt;
+use std::collections::VecDeque;
 use rand::Rng;
 
 #[derive(Debug, Eq, PartialEq, PartialOrd, Clone)]
@@ -11,7 +12,7 @@ pub struct Card {
 
 #[derive(Debug, Eq, PartialEq, PartialOrd, Clone)]
 pub struct Deck {
-    pub cards: Vec<Card>
+    pub cards: VecDeque<Card>
 }
 
 impl Card {
@@ -37,41 +38,47 @@ impl fmt::Display for Card {
 }
 
 impl Deck {
-    pub fn new_from_vec(input: Vec<Card>) -> Deck {
+    pub fn new_from_vec_deque(input: VecDeque<Card>) -> Deck {
         Deck { cards: input }
     }
 
     pub fn make_standard() -> Deck {
-        let mut result = Vec::new();
+        let mut result = VecDeque::new();
         let suits  = &["♣", "♦", "♥", "♠"];
         let values = &["2", "3", "4", "5", "6", "7", "8", "9", "10",
                        "Jack", "Queen", "King", "Ace"];
 
         for suit in suits {
             for value in values {
-                result.push(Card::new_from_str(*suit, value));
+                result.push_back(Card::new_from_str(*suit, value));
             }
         }
-        return Deck::new_from_vec(result);
+        return Deck::new_from_vec_deque(result);
     }
 
     pub fn make_fifth_dimension() -> Deck {
-        let mut result = Vec::new();
+        let mut result = VecDeque::new();
         let suits  = &["♣", "♦", "♥", "♠", "★"];
         let values = &["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
                        "Jack", "Princess", "Queen", "King", "Joker"];
 
         for suit in suits {
             for value in values {
-                result.push(Card::new_from_str(*suit, value));
+                result.push_back(Card::new_from_str(*suit, value));
             }
         }
-        return Deck::new_from_vec(result);
+        return Deck::new_from_vec_deque(result);
     }
 
     // destructively shuffles the internal vector
     pub fn shuffle(&mut self) {
-        rand::thread_rng().shuffle(&mut self.cards);
+        // shuffle isn't defined on VecDeques, so it's manually defined here.
+        let mut i = self.len();
+        while i > 2 {
+            i -= 2;
+            let j = rand::thread_rng().gen_range(0, i + 1);
+            self.cards.swap(i, j)
+        }
     }
 
     // returns a new shuffled deck
@@ -85,8 +92,20 @@ impl Deck {
         self.cards.len()
     }
 
-    pub fn pop(&mut self) -> Option<Card> {
-        self.cards.pop()
+    pub fn push_front(&mut self, card: Card) {
+        self.cards.push_front(card);
+    }
+
+    pub fn push_back(&mut self, card: Card) {
+        self.cards.push_back(card);
+    }
+
+    pub fn pop_front(&mut self) -> Option<Card> {
+        self.cards.pop_front()
+    }
+
+    pub fn pop_back(&mut self) -> Option<Card> {
+        self.cards.pop_back()
     }
 }
 
